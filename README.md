e585aa205afb2b1a898cc989d04ff4ea912bd270
Lucas <lucas_repetto@hotmail.com>
2021-05-04 12:56:04 -0400

    feat: requests upgrade

--- README.md
+++ README.md
@@ -3,13 +3,21 @@
 Initial configuration for development projects with webpack.
 
 ### Notas:
+
 Rebuild Node.js modules
 
 ```
 npm install
-``` 
+```
 
 Build the app
+
 ```
 npm run build
+```
+
+Run the server
+
+```
+npm start
 ```
--- src/index.js
+++ src/index.js
@@ -8,24 +8,31 @@ relativeTime();
 
 // Concatenacion y referencia URL transacciones ult. 24 horas
 let cryptoMarket = ["BTC", "ETH", "LTC", "BCH"];
+let currencyType = ["CLP", "COP", "PEN", "ARS"];
 const myList = document.querySelector("ul");
 
-// Fetch de URL
+// Recorrido de arreglos para la URL relativa
 for (let crypto of cryptoMarket) {
-  myRequest = `https://www.buda.com/api/v2/markets/${crypto}-CLP/trades?last_timestamp=${relativeTime}`;
-  fetch(myRequest)
-    .then((response) => response.json())
-    .then((data) => {
-      let max = 0;
-      for (const entries of data.trades.entries) {
-        if (max < entries[2]) {
-          max = entries[2];
+  for (let currency of currencyType) {
+    // Fetch de URL concatenada
+    myRequest = `https://www.buda.com/api/v2/markets/${crypto}-${currency}/trades?last_timestamp=${relativeTime}`;
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((data) => {
+        // Busqueda de la transaccion mas grande
+        let max = 0;
+        for (const entries of data.trades.entries) {
+          if (max < entries[2]) {
+            max = entries[2];
+          }
         }
-      }
-      let listItem = document.createElement("li");
-      listItem.append(
-        `La transaccion de ${crypto} de mayor valor realizada en las últimas 24 horas fue de: $${max}`
-      );
-      myList.appendChild(listItem);
-    });
+        // Insercion al DOM
+        let listItem = document.createElement("li");
+        listItem.className = "list-group-item";
+        listItem.append(
+          `En el mercado ${currency}, La transaccion de ${crypto} de mayor valor en las últimas 24 horas fue de: $${max}`
+        );
+        myList.appendChild(listItem);
+      });
+  }
 }
