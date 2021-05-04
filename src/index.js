3cbd86b1a46e583de7425b68649430be1eb66171
Lucas <lucas_repetto@hotmail.com>
2021-05-04 01:15:19 -0400

    feat: fetch & validations

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 // Calculo de Timestamp hace 24 horas
-const calcularTiempo = () => {
+const relativeTime = () => {
   var ts = Math.round(new Date().getTime() / 1000);
   var tsYesterday = ts - 24 * 3600;
-  console.log(`Timestamp hace 24 horas: ${tsYesterday}`);
+  return tsYesterday;
 };
-calcularTiempo();
+relativeTime();
 
 // Concatenacion y referencia URL transacciones ult. 24 horas
-const bchClp = `https://www.buda.com/api/v2/markets/BTC-CLP/trades?last_timestamp=${calcularTiempo}`;
-// Todo: Recorrer Arreglo ult. 24 horas
+let cryptoMarket = ["BTC", "ETH", "LTC", "BCH"];
+const myList = document.querySelector("ul");
 
 // Fetch de URL
-fetch(bchClp)
-  .then((response) => response.json())
-  .then((data) => console.log(`Transaccion de BCH: ${data.trades.entries}`));
+for (let crypto of cryptoMarket) {
+  myRequest = `https://www.buda.com/api/v2/markets/${crypto}-CLP/trades?last_timestamp=${relativeTime}`;
+  fetch(myRequest)
+    .then((response) => response.json())
+    .then((data) => {
+      let max = 0;
+      for (const entries of data.trades.entries) {
+        if (max < entries[2]) {
+          max = entries[2];
+        }
+      }
+      let listItem = document.createElement("li");
+      listItem.append(
+        `La transaccion de ${crypto} de mayor valor realizada en las últimas 24 horas fue de: $${max}`
+      );
+      myList.appendChild(listItem);
+    });
+}
